ae3fc5e3f769b8fe847d4a7ce484271e54db6c03
Roberto Esteban <45260992+estebanolroberto@users.noreply.github.com>
2024-02-20 09:07:40 +0100

    JavaScript p1

---
+++ RepasoWeb/aprendiendo_JavaScript/main.js
@@ -0,0 +1,10 @@
+
+
+var nombre= "Roberto Esteban"
+var altura = 180;
+
+
+var concatenacion = nombre + " " + altura;
+
+var datos = document.getElementById("datos");
+datos.innerHTML = concatenacion;
